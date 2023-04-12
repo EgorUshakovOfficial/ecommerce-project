df2a09c7db0e2f2e7d68cbda698e9bf62435f61a
Egor Ushakov <egorushakov13@gmail.com>
2023-04-12 14:25:31 -0600

    Added CORS

--- server/project/project/settings.py
+++ server/project/project/settings.py
@@ -22,6 +22,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+# CORS configuration
+CORS_ORIGIN_WHITELIST = ['http://localhost:3000']
 
 # Application definition
 INSTALLED_APPS = [
@@ -43,6 +45,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware'
 ]
 
 ROOT_URLCONF = 'project.urls'
