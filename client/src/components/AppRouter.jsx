9ca4db15b5910e101ec10fcdaf0f766683054397
Egor Ushakov <egorushakov13@gmail.com>
2023-06-12 10:54:51 -0600

    Added protectd route functionality

--- client/src/components/AppRouter.jsx
+++ client/src/components/AppRouter.jsx
@@ -1,14 +1,24 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import PrivateRoute from './PrivateRoute';
+import { useSelector } from 'react-redux';
+import { BrowserRouter as Router, Routes, Route, Navigate} from 'react-router-dom';
 import { Checkout, Confirmation, Home, ProductPage } from '../pages';
 
 export default function AppRouter(){
+    // User state
+    const user = useSelector(state => state.user.data);
+
+    // Checks if the user is authenticated
+    const isAuthenticated = (user !== null);
+
     return (
         <Router>
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/success" element={<Confirmation />} />
-                <PrivateRoute path="/checkout/:section" element={<Checkout />} />
+                <Route path="/checkout/:section" element={
+                    isAuthenticated ? (
+                    <Checkout /> ) :
+                    (<Navigate to="/" replace={true} />)}
+                />
                 <Route path="/product-page/:productId" element={<ProductPage />} />
             </Routes>
         </Router>
--- client/src/components/index.js
+++ client/src/components/index.js
@@ -4,7 +4,6 @@ import Button from './utils/Button';
 import Image from './utils/Image';
 import Loading from './utils/Loading';
 import Logo from './Logo';
-import PrivateRoute from './PrivateRoute';
 import Ratings from './Ratings';
 
 export {
@@ -14,6 +13,5 @@ export {
     Image,
     Loading,
     Logo,
-    PrivateRoute,
     Ratings
 };
